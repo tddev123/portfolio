9fabfe281764c01afe0f0eff08af3aaa1a37df5f
tddev123 <154409493+tddev123@users.noreply.github.com>
2024-07-28 17:57:55 -0700

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,27 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer class="bg-gray-700 text-white p-2 flex ">
-    <div class="container mx-auto flex justify-between items-center">
-
-
-    <img className="size-12 " src=".\images\icons8-t-80 (1).png" alt="" />
-    <nav className="pl-32">
-            
-            <a href="#" class="text-xl mx-2">Contact Me - Tyler26daniel@gmail.com </a>
+    <footer className="bg-gray-700  text-white p-2">
+      <div className="container mx-auto flex justify-between items-center">
+        <img className="w-12 h-12" src="./images/icons8-t-80 (1).png" alt="" />
+        <nav className="pl-8 md:pl-32">
+          <a href="#" className="text-lg md:text-xl mx-2 block">Contact Me - Tyler26daniel@gmail.com</a>
         </nav>
+        <p className="text-sm">&copy; 2024 My Website. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-        
-
-
-        <p class="text-sm">&copy; 2024 My Website. All rights reserved.</p>
-
-        
-      
-    </div>
-</footer>
-  )
-}
-
-export default Footer
+export default Footer;
