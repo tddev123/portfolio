e166c8a28bb33e296bb987ba5d0801bbf19cb654
tddev123 <154409493+tddev123@users.noreply.github.com>
2024-07-28 17:56:58 -0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Nav } from "./Nav";
+import Nav from "./Nav";
 
 export const Headerr = () => {
   return (
-    <header className=" z-30  sticky top-0  mx-auto  flex w-full max-w-7xl bg-white flex-wrap items-center justify-between border-b border-gray-100 bg-background p-5 font-sans font-bold uppercase text-text-primary dark:border-gray-800  dark:text-d-text-primary">
-      <h1 className="text-xl">Tyler Altenburg</h1>
-
-      <Nav />
+    <header className="z-30 sticky top-0 mx-auto flex w-screen  bg-white flex-wrap items-center justify-between border-b border-gray-100 p-5 font-sans font-bold uppercase text-text-primary dark:border-gray-800 dark:text-d-text-primary">
+      <a href="/">
+        <h1 className="text-xl">Tyler Altenburg</h1>
+      </a>
+      <div className="flex-grow flex justify-end">
+        <Nav />
+      </div>
     </header>
   );
 };
