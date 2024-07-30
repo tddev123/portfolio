e36dfec68305b54e538d7934733361104c3c2e95
tddev123 <154409493+tddev123@users.noreply.github.com>
2024-07-29 21:00:04 -0700

    Update CarsNav.jsx

--- src/components/CarsNav.jsx
+++ src/components/CarsNav.jsx
@@ -3,9 +3,9 @@ import CarsDropdown from './CarsDropdown'
 
 function CarsNav() {
   return (
-    <nav className=" justify-end ">
-    <div className="flex space-x-5 ">
-      <a  className=' text-white hover:text-blue-700' href="/"> Back To Porfolio </a>
+    <nav className=" md:justify-end ">
+    <div className="justify-end space-x-5 ">
+      <a  className=' text-white hover:text-blue-700' href="/Projects"> BACK TO PROJECTS </a>
       <a className=' hover:text-blue-700' href="/About">  </a>
       <a className=' hover:text-blue-700' href="/Projects">  </a>
     
