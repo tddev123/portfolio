07ffd5b347dba187d4f90add84b6b2a946e28688
tddev123 <154409493+tddev123@users.noreply.github.com>
2024-07-29 20:59:44 -0700

    Update CarsHeader.jsx

--- src/components/CarsHeader.jsx
+++ src/components/CarsHeader.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import CarsNav from './CarsNav'
-
+import React from 'react';
+import CarsNav from './CarsNav';
 
 function CarsHeader() {
   return (
-    <header className=" z-30  sticky top-0  mx-auto  flex w-full max-w-7xl bg-red-500  flex-wrap items-center justify-between border-b border-gray-100 bg-background p-4 font-sans font-bold uppercase text-text-primary dark:border-gray-800  dark:text-d-text-primary">
-      <div className='flex row-auto'><a href='./Houses'><img className='p-0 size-14 bg-white border-violet-600 border-2 m-0' src=".\images\pngtree-wash-car-logo-design-png-image_5820924.png" alt=""  /></a>  <h1 className=' font-serif text-white text-4xl pt-2 pl-6'>UCars</h1> </div> 
-      <form className='pl-48'> <h1 className='pr-28 text-2xl text-white'>Call to rent 954-305-0786</h1>
-        
-        
-        </form>
-
-        
-       
-
-      <CarsNav/>
+    <header className="z-30 sticky top-0 mx-auto flex w-full max-w-7xl bg-gray-800 flex-wrap items-center justify-between border-b border-gray-100 bg-background p-4 font-sans font-bold uppercase text-text-primary dark:border-gray-800 dark:text-d-text-primary">
+      <div className="flex items-center space-x-4">
+        <a href='./Houses'>
+          <img className="p-0 w-14 h-14 bg-white border-violet-600 border-2" src="./images/pngtree-wash-car-logo-design-png-image_5820924.png" alt="Car Logo" />
+        </a>
+        <h1 className="font-serif text-white text-4xl">RCars</h1>
+      </div>
       
+      <div className="hidden md:block">
+        <h1 className="text-2xl text-white">Call to rent 954-305-0786</h1>
+      </div>
+      
+      <CarsNav />
+
+      <div className="block md:hidden mt-4 w-full text-center">
+        <h1 className="text-xl text-white">Call to rent 954-305-0786</h1>
+      </div>
     </header>
-  )
+  );
 }
 
-export default CarsHeader
+export default CarsHeader;
