7f7f01ac5ed14902714d07a4ae77be0967125cef
tddev123 <154409493+tddev123@users.noreply.github.com>
2025-12-31 00:03:35 -0500

    Update contact email in Footer component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,12 +6,13 @@ const Footer = () => {
       <div className="container mx-auto flex justify-between items-center">
         <img className="w-12 h-12" src="./images/icons8-t-80 (1).png" alt="" />
         <nav className="pl-8 md:pl-32">
-          <a href="#" className="text-lg md:text-xl mx-2 block">Contact Me - Tyler26daniel@gmail.com</a>
+          <a href="#" className="text-lg md:text-xl mx-2 block">Contact Me - tdengineer123@gmail.com</a>
         </nav>
-        <p className="text-sm">&copy; 2024 My Website. All rights reserved.</p>
+        <p className="text-sm"></p>
       </div>
     </footer>
   );
 };
 
 export default Footer;
+
