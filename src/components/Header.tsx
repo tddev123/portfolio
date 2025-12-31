d1b2fd1550cb9a10f5dd8d5774698c2cb4a1bc57
tddev123 <154409493+tddev123@users.noreply.github.com>
2025-12-31 01:27:47 -0500

    Fix formatting and clean up Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 export const Headerr = () => {
   return (
-    <header className=""z-30 sticky top-0 mx-auto flex w-screen  bg-white flex-wrap items-center justify-between p-3 font-sans font-bold uppercase text-text-primary dark:text-d-text-primary">
+    <header className=""z-30 sticky top-0 mx-auto flex w-screen  bg-white flex-wrap items-center justify-between p-3 font-sans font-bold uppercase">
       <a href="/">
         <h1 className="text-xl">Tyler Altenburg</h1>
       </a>
@@ -15,5 +15,6 @@ export const Headerr = () => {
 };
 
 export default Headerr;
+
 
 
