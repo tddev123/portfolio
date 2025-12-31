b4442e48c3b09784cfd9eb94865f9a126803340f
tddev123 <154409493+tddev123@users.noreply.github.com>
2025-12-31 00:17:37 -0500

    Refactor Header component with style adjustments
    Updated the Header component styles and reduced padding.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 export const Headerr = () => {
   return (
-    <header className="z-30 sticky top-0 mx-auto flex w-screen  bg-white flex-wrap items-center justify-between border-b border-gray-100 p-5 font-sans font-bold uppercase text-text-primary dark:border-gray-800 dark:text-d-text-primary">
+    <header className=""z-30 sticky top-0 mx-auto flex w-screen  bg-white flex-wrap items-center justify-between p-3 font-sans font-bold uppercase text-text-primary dark:border-gray-800 dark:text-d-text-primary">
       <a href="/">
         <h1 className="text-xl">Tyler Altenburg</h1>
       </a>
@@ -15,3 +15,4 @@ export const Headerr = () => {
 };
 
 export default Headerr;
+
